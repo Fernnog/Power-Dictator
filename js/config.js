cd31f741252afc3391a9bcf428e0496a50d17a1d
Fernnog <fernandocnogueira@gmail.com>
2025-12-10 10:41:16 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,20 @@
+export const CONFIG = {
+    STORAGE_KEYS: {
+        TEXT: 'ditado_backup_text',
+        MIC: 'ditado_pref_mic',
+        API: 'ditado_digital_gemini_key'
+    },
+    AUDIO: {
+        HIGHPASS_FREQ: 85,
+        COMPRESSOR_THRESHOLD: -50
+    },
+    SHORTCUTS: {
+        REC: 'KeyG',
+        CLEAR: 'KeyL',
+        COPY: 'KeyC',
+        UNDO: 'KeyZ'
+    },
+    UI: {
+        TOAST_DURATION: 5000
+    }
+};
